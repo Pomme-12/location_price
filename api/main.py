2f99ab87c84a44aa2a8a1bd414270a67f8f04159
Pomme_12 <pomane98@gmail.com>
2026-06-23 20:54:56 +0000

    PR

--- api/main.py
+++ api/main.py
@@ -2,7 +2,21 @@ from fastapi import FastAPI
 
 app= FastAPI()
 @app.get("/")
+
 def read_root():
     return{
         "message": "Hello From FastAPI"
+    }
+
+@app.get("/hello")
+def read_root():
+    return{
+        "message": "Hello Routers"
+    }
+
+
+@app.get("/tour")
+def read_root():
+    return{
+        "message": "Hello Tours"
     }
